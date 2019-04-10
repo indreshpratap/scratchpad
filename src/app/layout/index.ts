2df8a52ded1d2ddacd9ba84871336e83d1dbf9d8
Indresh Pratap Singh <indreshpratap.singh@ihsmarkit.com>
2019-04-10 21:07:33 +0530

    main screen

--- src/app/layout/index.ts
+++ src/app/layout/index.ts
@@ -1,5 +1,9 @@
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RightSideBarComponent } from './right-side-bar/right-side-bar.component';
 
 export const LAYOUTS = [
-    TopHeaderComponent
+    TopHeaderComponent,
+    FooterComponent,
+    RightSideBarComponent
 ];
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -9,17 +9,19 @@ export class HomePageComponent implements OnInit {
 
   imageCards = []
   imgPath = 'assets/images/';
+  
   constructor() { }
 
   ngOnInit() {
+    this.fetchImageCards();
   }
 
   fetchImageCards() {
     this.imageCards = [
-      { image:'america.jpg', topTag:'SURPRIZE ME', bottomTagline:' 8 Secret Place to Explore in Thailand' },
-      { image:'africa.jpg', topTag:'BEACH', bottomTagline:' 6 Secret Place to Explore in Thailand' },
-      { image:'asia.jpg', topTag:'SURPRIZE ME', bottomTagline:' 8 Secret Place to Explore in Thailand' },
-      { image:'europe.jpg', topTag:'SURPRIZE ME', bottomTagline:' 8 Secret Place to Explore in Thailand' },
+      { image: 'america.jpg', topTag: 'SURPRIZE ME', bottomTagline: ' test is fsld  k dsklfjskdl jslflkfjfsdlkfjds lk dlskj sdlkf flksdj lks fjlfjsk  8 Secret Place to Explore in Thailand' },
+      { image: 'africa.jpg', topTag: 'BEACH', bottomTagline: ' 6 Secret Place to Explore in Thailand' },
+      { image: 'asia.jpg', topTag: 'SURPRIZE ME', bottomTagline: ' 8 Secret Place to Explore in Thailand' },
+      { image: 'europe.jpg', topTag: 'SURPRIZE ME', bottomTagline: ' 8 Secret Place to Explore in Thailand' },
     ];
   }
 
